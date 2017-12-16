585eee82e60c312e5254db9fed9d5dc4507745bc
choudharyRahul22 <32006288+choudharyRahul22@users.noreply.github.com>
2017-12-16 12:19:33 +0530

    angular material support added

---
+++ SourceCode/Frontend/admin-portal/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
